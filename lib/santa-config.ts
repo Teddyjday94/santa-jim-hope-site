0c4ff9a514bfcd2d700bfafbe8f3cf150d98fe93
Teddyjday94 <130117910+Teddyjday94@users.noreply.github.com>
2026-09-17 00:10:39 -0500

    feat: add Santa scheduler backend config

---
+++ lib/santa-config.ts
@@ -0,0 +1,4 @@
+export const SANTA_SUPABASE_URL = "https://taskvfzqgpqxpdifmxru.supabase.co";
+export const SANTA_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_q640ynIE4XmfGhA1lostTA_RUF_NDQG";
+export const SANTA_SITE_ID = "a0d14a7f-7e08-4042-a6c9-0f83c03eefb3";
+export const SANTA_TEST_NOTIFICATION_EMAIL = "thomasdbiz26@gmail.com";

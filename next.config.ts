cd7ab2a9f75b42eb7a8bd79dead10763de41a660
Teddyjday94 <130117910+Teddyjday94@users.noreply.github.com>
2026-09-21 17:11:33 -0500

    Add new Santa Jim gallery photos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d2ol7oe51mr4n9.cloudfront.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
